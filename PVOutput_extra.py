9b2b57c1dd3670ea10eecdfa66232f91dae8da00
Ron Hermsen <ronhermsen@gmail.com>
2024-01-08 22:05:58 +0100

    Flexible data upload

--- PVOutput_extra.py
+++ PVOutput_extra.py
@@ -7,8 +7,13 @@ Created on Fri Aug 25 23:30:15 2023
 
 ToDo:
     - Verify if HomeWizard support mDNS (it should), and look to support this.
+    - Include livingroom temperature, obtained external (AirGradient) in the extended data (v9).
 
 Done:
+    2024-01-08:
+    - Allow a flexible use of parameters to be send to PVOutput, omid parameters with a none-type.
+    2024-01-07:
+    - Upload delta gas consumption as v8 (extended data).
     2024-01-06:
     - Upload gas consumtion as v7 (extended data).
     2024-01-05:
@@ -22,6 +27,7 @@ from homewizard_energy import HomeWizardEnergy
 from homewizard_energy.models import Device, Data
 import asyncio
 import aiohttp
+import json
 from datetime import datetime
 import os
 
@@ -32,10 +38,13 @@ HOMEWIZARD_IP = '192.168.188.102'
 
 class PVOutput(object):
     """
-    Send the following information to PVOutput.org every send_interval (5 min).
-    v6 = Voltage (Line3)
+    Send the following information to PVOutput.org.
+    
     v3 = Energy [Wh] Consumption, a cumulative value in Wh
     v4 = Power [W] Consumption, current power consumption in W
+    v6 = Voltage (os.environ['SOLAR_LINE']), highest voltage if multiple lines are in us
+    v7 = Gas [m3] Consumption, a cumulative value in m3
+    v8 = Gas [m3] Consumption, increase from previous measurement in m3
     
     Ref:
         https://pvoutput.org/help/api_specification.html#add-status-service
@@ -43,9 +52,9 @@ class PVOutput(object):
     c1 = 3 (need to omit this flag becasue n=1.)
     n = 1
     """
-    def __init__(self, timestamp, voltage, energy, power, gas):
+    def __init__(self, timestamp, voltage=None, energy=None, power=None, gas=None, delta_gas=None):
         """
-        Read the required data from HomeWizard and send it to PVOutput.
+        Paramters to send to PVOutput.
         """
         self.pvoutput_systemid = os.environ['PVO_SYSTEMID']
         self.pvoutput_apikey = os.environ['PVO_APIKEY']
@@ -54,17 +63,38 @@ class PVOutput(object):
         self.energy = energy
         self.power = power
         self.gas = gas
+        self.delta_gas = delta_gas
 
     async def uploadData(self):
+        """
+        Upload the measurement paramters.
+        """
+        # pvoutputdata = {
+        # 'd': self.timestamp.strftime("%Y%m%d"),
+        # 't': self.timestamp.strftime("%H:%M"),
+        # 'v3': str(self.energy),
+        # 'v4': str(self.power),
+        # 'v6': str(self.voltage),
+        # 'v7': str(self.gas),
+        # 'v8': str(self.delta_gas),
+        # 'n': '1'
+        # }
+
         pvoutputdata = {
-        'd': self.timestamp.strftime("%Y%m%d"),
-        't': self.timestamp.strftime("%H:%M"),
-        'v3': str(self.energy),
-        'v4': str(self.power),
-        'v6': str(self.voltage),
-        'v7': str(self.gas),
-        'n': '1'
+                'd': self.timestamp.strftime("%Y%m%d"),
+                't': self.timestamp.strftime("%H:%M")
         }
+        if self.voltage != None:
+            pvoutputdata['v6'] = self.voltage
+        if self.energy != None:
+            pvoutputdata['v3'] = self.energy
+        if self.power != None:
+            pvoutputdata['v4'] = self.power
+        if self.gas != None:
+            pvoutputdata['v7'] = self.gas
+        if self.delta_gas != None:
+            pvoutputdata['v8'] = self.delta_gas
+        pvoutputdata['n'] = '1'
 
         headerspv = {
             'X-Pvoutput-SystemId': self.pvoutput_systemid,
@@ -81,10 +111,15 @@ class PVOutput(object):
 
 
 class HomeEnergy(object):
-    def __init__(self):
+    """
+    Obtain measurement values from the P1 smartmeter using HomeWizard P1 meter.
+    All device information and measurement data is requested.
+    """
+    def __init__(self, old_gas):
         self.device = None
         self.data = None
         self.last_data = None
+        self.old_gas = old_gas
         self.line = os.environ['SOLAR_LINE']
         #self.line = '123'
     
@@ -145,16 +180,47 @@ class HomeEnergy(object):
         v7, Total cumulative [m3] imported gas.
         Extended data which needs configured via Web-GUI e.g.:
         Extended Data > v7: 
-            color : #fe0071, Area
+            color : #c9244d, Area
+            Label: Gas, Unit: m3, 
+            Axis: 0, Summary: Change
+            Credit/Debit: Debit
+            + > Calculate as: Uncumulate
+        """
+        return self.data.total_gas_m3
+    
+    def get_delta_gas_consumption(self):
+        """
+        v8, Gas usage in the last 5 minutes.
+        Extended data which needs configured via Web-GUI e.g.:
+        Extended Data > v8: 
+            color : #fe0071, Line
             Label: Gas, Unit: m3, 
             Axis: 0, Summary: Change
             Credit/Debit: Debit
         """
-        return self.data.total_gas_m3
+        if self.old_gas == None:
+            return 0
+        else:
+           return self.data.total_gas_m3 - self.old_gas
+
+
+class AirGradient(object):
+    """
+    """
+    def __init__(self):
+        self.url = "http://192.168.188.100:8080/metricsjson"
+    
+    async def get_temp(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url) as response:
+                json_data = await response.text()
+                data_dict = json.loads(json_data)
+                return data_dict["temp"]
 
 
 async def main():
-    print(f'PVOutput.py has started at {datetime.now().strftime("%Y%m%d %H:%M:%S")}, and should run infinity.')  
+    print(f'PVOutput.py has started at {datetime.now().strftime("%Y%m%d %H:%M:%S")}, and should run infinity.')
+    old_gas = None
     while True:
         now = datetime.now()
         delay = (4 - (now.minute % 5)) * 60 + (30 - now.second)
@@ -163,27 +229,33 @@ async def main():
         #print("Delay =", delay, "Time now =", now.strftime("%Y%m%d %H:%M:%S"))
         await asyncio.sleep(delay)        
         try:
-            getData = HomeEnergy()
+            getData = HomeEnergy(old_gas)
             await asyncio.gather(getData.contactHW(HOMEWIZARD_IP))
-            print("Data obtained at:", getData.get_collection_timestamp(), "V6_Voltage:", getData.get_pv_line_voltage(),"V1_Energy_gen:", getData.get_energy_generation(), "V3_Energy:", getData.get_energy_consumption(), "V4_Power:", getData.get_power_consumption(), "V7_Gas:", getData.get_gas_consumption())
+            print("Data obtained at:", getData.get_collection_timestamp(), 
+                  "V6_Voltage:", getData.get_pv_line_voltage(),
+                  "V1_Energy_gen:", getData.get_energy_generation(), 
+                  "V3_Energy:", getData.get_energy_consumption(), 
+                  "V4_Power:", getData.get_power_consumption(), 
+                  "V7_Gas:", getData.get_gas_consumption(),
+                  "v8_Gas delta", getData.get_delta_gas_consumption())
+            # getTemp = AirGradient()
+            # await asyncio.gather(getTemp.get_temp())
         except Exception as e:
             exception_time = datetime.now().strftime("%Y%m%d %H:%M:%S")
             print(f"Failed reading HomeEnergy at {exception_time} - ErrorType : {type(e).__name__}, Error : {e}")
-        # except:
-        #     print(f'Failed reading HomeEnergy at {datetime.now().strftime("%Y%m%d %H:%M:%S")}.')
         else:
             try:
                 sendData = PVOutput(getData.get_collection_timestamp(), 
                                     getData.get_pv_line_voltage(),
                                     getData.get_energy_consumption(),
                                     getData.get_power_consumption(),
-                                    getData.get_gas_consumption())
+                                    getData.get_gas_consumption(),
+                                    getData.get_delta_gas_consumption())
                 await asyncio.gather(sendData.uploadData())
+                old_gas = getData.get_gas_consumption()
             except Exception as e:
                 exception_time = datetime.now().strftime("%Y%m%d %H:%M:%S")
                 print(f"Failed to send data at {exception_time} - ErrorType : {type(e).__name__}, Error : {e}")
-            # except:
-            #     print(f'Failed to send data at {datetime.now().strftime("%Y%m%d %H:%M:%S")}')
         await asyncio.sleep(5)
     print("Done")
 
